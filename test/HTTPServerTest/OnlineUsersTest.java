1a6fa9037d8ba8cc5cafd903d3f1f87dce9ad124
TeodorDumitru <TeoDum@gmail.com>
2014-09-10 10:43:51 +0200

    Created empty OnlineUsersTest class.
    Extreme programming: Martin & Teo

---
+++ test/HTTPServerTest/OnlineUsersTest.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package HTTPServerTest;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Teo
+ */
+public class OnlineUsersTest {
+    
+    public OnlineUsersTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+}
